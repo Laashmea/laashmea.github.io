e6ccc684cc2d28c4a9ff3b2321016197ac5f97d0
Leslie <leslie@Leslies-MacBook-Pro.local>
2019-03-03 02:58:20 -0500

    Created new components and updated them with old content

--- personal/src/app/app-routing.module.ts
+++ personal/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component'
-import { AboutComponent} from './about/about.component'
+import { HomeComponent } from './home/home.component';
+import { AboutComponent} from './about/about.component';
+import { ResumeComponent} from './resume/resume.component';
+import { ContactComponent} from './contact/contact.component';
 
 const routes: Routes = [
   {
@@ -11,7 +13,15 @@ const routes: Routes = [
   {
     path: 'about',
     component: AboutComponent
-  }
+  },
+  {
+    path: 'resume',
+    component: ResumeComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
 ];
 
 @NgModule({
